0cb832aab7af8d51d434c168259e4caffcc4ceeb
D'CODER <60844924+jonelnaquita@users.noreply.github.com>
2023-10-26 16:47:10 +0800

    add mysql

--- app.py
+++ app.py
@@ -36,9 +36,10 @@ app = Flask(__name__,
 
 app.secret_key = 'xyzsdfg'
 
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''   
+app.config['MYSQL_HOST'] = 'mysql-152093-0.cloudclusters.net'
+app.config['MYSQL_PORT'] = 19876
+app.config['MYSQL_USER'] = 'admin'
+app.config['MYSQL_PASSWORD'] = 'redchatbot'   
 app.config['MYSQL_DB'] = 'redcms'
 
 mysql = MySQL(app)
