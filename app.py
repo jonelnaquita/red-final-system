dfdf7bfcaf26e0490caf270d52b0af4258a85d6d
D'CODER <60844924+jonelnaquita@users.noreply.github.com>
2023-12-06 11:00:10 +0800

    Update app.py
    update the post prompt

--- app.py
+++ app.py
@@ -836,7 +836,7 @@ def dialogflow_webhook():
         start_time = datetime.now()
 
         query = str(data['text'])
-        postPrompt = str(query + " Don’t give me answer NOT AVAILABLE from your context.")
+        postPrompt = str(query + " Do not give me any information about procedures and service features that are not mentioned in the PROVIDED CONTEXT.")
 
         # Get conversation string before updating
         conversation_string_before = get_conversation_string(sessionID)
